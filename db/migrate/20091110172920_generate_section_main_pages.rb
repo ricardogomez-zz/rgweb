1ba0e0d6da105aeda456b733c566ecb87db961e5
dani <danigb@gmail.com>
2009-11-10 18:37:15 +0100

    migrations

--- db/migrate/20091110172920_generate_section_main_pages.rb
+++ db/migrate/20091110172920_generate_section_main_pages.rb
@@ -2,7 +2,6 @@ class GenerateSectionMainPages < ActiveRecord::Migration
   def self.up
     ApplicationController::SECTIONS.each do |section|
       page = Page.find_by_name(section)
-      page.update_attribute(:title, section) if page.title.nil?
       Page.create!(:name => section, :section => section) if page.nil?
     end
   end
