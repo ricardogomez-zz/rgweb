8288f66fa652bbdb2e64dc04c719f462496d7fec
dani <danigb@gmail.com>
2009-11-12 16:41:44 +0100

    secciones pequeñas

--- app/controllers/ver_controller.rb
+++ app/controllers/ver_controller.rb
@@ -2,19 +2,25 @@ class VerController < ApplicationController
 	layout 'ver'
 	
 	def index
-#		params[:section] = 'inicio'
-#		show
-#		render :action => 'show'
+    @section = 'inicio'
+    #		params[:section] = 'inicio'
+    #		show
+    #		render :action => 'show'
     render :action => 'index', :layout => 'nada'
 	end
 
 	def show
-		section = params[:section]
-		name = params[:page] || section
-		@page = Page.find_by_section_and_name(section, name) || Page.new
-		if @page.show_thumnails? || @page.show_list?
-			@pages = Page.find_all_by_section(section, :order => 'position') 
-			@subsection = ""
-		end
+		@section = params[:section]
+    if 'inicio' == params[:section]
+      redirect_to :action => 'index'
+    else
+      name = params[:page] || @section
+
+      @page = Page.find_by_section_and_name(@section, name) || Page.new
+      if @page.show_thumnails? || @page.show_list?
+        @pages = Page.find_all_by_section(@section, :order => 'position')
+        @subsection = ""
+      end
+    end
 	end	
 end
