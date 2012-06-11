506f30d9ff5793eb34bc21101ddd4021d42fddab
danigb <danigb@gmail.com>
2012-06-11 22:18:32 +0200

    Add encoding. Postgres schema dump. Remove user model

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class AttachmentsController < ApplicationController
 
 	def delete_image
--- app/controllers/editar_controller.rb
+++ app/controllers/editar_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class EditarController < ApplicationController
   before_filter :login_required
   
